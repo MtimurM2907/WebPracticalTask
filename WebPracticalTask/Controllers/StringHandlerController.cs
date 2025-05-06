2f1921030903ce6fc7a6312d67468954626803d1
MtimurM2907 <timur_mmm00@mail.ru>
2025-05-06 13:08:06 +0500

    Добавьте файлы проекта.

---
+++ WebPracticalTask/Controllers/StringHandlerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPracticalTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringHandlerController : ControllerBase
+    {
+        [HttpGet]
+        public void GetString(string text)
+        {
+
+        }
+    }
+}
